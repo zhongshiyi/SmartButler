52e394f23b025b92390c5b7afcbb41444e0edb39
zhongshiyi <1351896179@qq.com>
2019-07-20 14:48:25 +0800

    百度定位，并且绘制了卫星图，路况图和热力分布图

--- app/src/main/java/com/example/smartbutler/ui/LocationActivity.java
+++ app/src/main/java/com/example/smartbutler/ui/LocationActivity.java
@@ -3,12 +3,27 @@ package com.example.smartbutler.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 import com.example.smartbutler.R;
+import com.example.smartbutler.utils.L;
 
 /**
  * 项目名：    SmartButler
@@ -19,6 +34,10 @@ import com.example.smartbutler.R;
  * 描述：      我的位置
  */
 public class LocationActivity extends BaseActivity {
+
+
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
 
 
     //声明百度地图主控件
@@ -38,9 +57,58 @@ public class LocationActivity extends BaseActivity {
         mMapView = findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
 
+        //声明LocationClient类
+        mLocationClient = new LocationClient(getApplicationContext());
+        //注册监听函数
+        mLocationClient.registerLocationListener(myListener);
+
+        //初始化定位参数
+        initLocation();
+
+        //mLocationClient为第二步初始化过的LocationClient对象
+        //调用LocationClient的start()方法，便可发起定位请求
+        mLocationClient.start();
 
 
+    }
 
+    //配置定位SDK参数(封装定位)
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        //可选，设置定位模式，默认高精度
+        //LocationMode.Hight_Accuracy：高精度；
+        //LocationMode. Battery_Saving：低功耗；
+        //LocationMode. Device_Sensors：仅使用设备；
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，设置返回经纬度坐标类型，默认GCJ02
+        //GCJ02：国测局坐标；
+        //BD09ll：百度经纬度坐标；
+        //BD09：百度墨卡托坐标；
+        //海外地区定位，无需设置坐标类型，统一返回WGS84类型坐标
+        option.setCoorType("bd09ll");
+        //可选，设置发起定位请求的间隔，int类型，单位ms
+        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
+        //如果设置非0，需设置1000ms以上才有效
+        option.setScanSpan(0);
+        //可选，设置是否使用gps，默认false
+        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
+        option.setOpenGps(true);
+        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+        option.setLocationNotify(true);
+        //可选，定位SDK内部是一个service，并放到了独立进程。
+        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+        option.setIgnoreKillProcess(false);
+        //可选，设置是否收集Crash信息，默认收集，即参数为false
+        option.SetIgnoreCacheException(false);
+        //可选，V7.2版本新增能力
+        //如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
+        option.setWifiCacheTimeOut(5 * 60 * 1000);
+        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+        option.setEnableSimulateGps(false);
+        //mLocationClient为第二步初始化过的LocationClient对象
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+        mLocationClient.setLocOption(option);
     }
 
     /**
@@ -91,6 +159,7 @@ public class LocationActivity extends BaseActivity {
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -101,8 +170,57 @@ public class LocationActivity extends BaseActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
         mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
     }
 
+
+    //4、实现BDAbstractLocationListener接口,即定位的回调
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取经纬度相关（常用）的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+            double latitude = location.getLatitude();    //获取纬度信息
+            double longitude = location.getLongitude();    //获取经度信息
+            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+
+            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+            String coorType = location.getCoorType();
+
+            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+            int errorCode = location.getLocType();
+
+            String addr = location.getAddrStr();    //获取详细地址信息
+            String country = location.getCountry();    //获取国家
+            String province = location.getProvince();    //获取省份
+            String city = location.getCity();    //获取城市
+            String district = location.getDistrict();    //获取区县
+            String street = location.getStreet();    //获取街道信息
+
+
+            //移动到我的位置
+            //设置缩放，确保屏幕内有我
+            MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.zoomTo(18);
+            mBaiduMap.setMapStatus(mapStatusUpdate);
+
+            //开始移动
+            MapStatusUpdate mapLatIng = MapStatusUpdateFactory.newLatLng(new LatLng(latitude, longitude));
+            mBaiduMap.setMapStatus(mapLatIng);
+
+            //定义Maker坐标点
+            LatLng point = new LatLng(latitude, longitude);
+            //构建Marker图标
+            BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.location);
+            //构建MarkerOption，用于在地图上添加Marker
+            OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
+            //在地图上添加Marker，并显示
+            mBaiduMap.addOverlay(option);
+        }
+    }
 }
